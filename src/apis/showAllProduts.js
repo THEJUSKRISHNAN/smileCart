2d60ac5a2bcc443613bcc2071c64c294f22b1138
THEJUSKRISHNAN <thejus12a2003@gmail.com>
2024-05-13 10:35:22 +0530

    commit

--- src/apis/showAllProduts.js
+++ src/apis/showAllProduts.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const showAllProducts = (params) =>
-axios.get(`products?search_term=${params}`);
+axios.get(`products?search_term=${params}&page=1&page_size=50`);
 
   const AllProductsApi = { showAllProducts };
   export default AllProductsApi;
