f25255307d2b638693a58d3041fd8dca8287d9b6
THEJUSKRISHNAN <thejus12a2003@gmail.com>
2024-05-14 22:54:36 +0530

    commit

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react'
+const AddToCart = () => {
+
+    const [isInCart, setIsInCart] = useState(false)
+
+    const handleClick = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setIsInCart(prevValue => !prevValue);
+    };
+
+    return (
+        <button className=" pt-2 font-semibold bg-blue-700 text-white px-3 py-2 rounded-lg align-middle my-3 ml-[3rem]" onClick={handleClick}>
+            {isInCart ? "Remove from cart" : "Add to cart"}
+        </button>
+    )
+}
+
+export default AddToCart
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useState, useEffect } from "react"
-import { FaCartShopping } from "react-icons/fa6";
 import { CiSearch } from "react-icons/ci";
 import AllProductsApi from '../../src/apis/showAllProduts'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import Nav from './Nav'
 import useDebounce from '../hooks/useDebounce';
 import CartIcon from './CartIcon';
+import AddToCart from './AddToCart';
 
 
 const Products = () => {
@@ -38,11 +38,6 @@ const Products = () => {
                       onChange={event => setSearchKey(event.target.value)} />
                     <CiSearch className='absolute top-2 left-1' />
                 </div>
-
-                {/* <div className='relative flex pl-7'>
-                    <FaCartShopping className='text-4xl' />
-                    <span className='absolute bottom-8 right-0 font-bold '>0</span>
-                </div> */}
                 <CartIcon/>
             </div>} />
 
@@ -55,7 +50,7 @@ const Products = () => {
                             <p className="text-center pt-2 font-semibold">MRP:{item.mrp}</p>
                             <p className="text-center pt-2 font-semibold">Offer Price:{item.offer_price}</p>
                             <p className="text-center pt-2 font-semibold">Available Quantity:{item.available_quantity}</p>
-                            <button className=" pt-2 font-semibold bg-blue-700 text-white px-3 py-2 rounded-lg align-middle my-3 ml-[3rem]">Add to cart</button>
+                            <AddToCart/>
                         </div>
                     </Link>
                 })}
